15ac1bb0ef640a77cb03320c7f436800d72c07f9
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-09-25 11:14:00 +0700

    Create Lab9.cpp

---
+++ Lab9.cpp
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[100];   
+    int i, j;
+
+    printf("Enter N : ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("Enter value[%d] : ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Index:");
+    for (i = 0; i < n; i++) {
+        printf(" %d", i);
+    }
+    printf("\n");
+
+    printf("Array:");
+    for (i = 0; i < n; i++) {
+        if (arr[i] < 2) {
+            printf(" #");   
+        } else {
+            for (j = 2; j * j <= arr[i]; j++) {
+                if (arr[i] % j == 0) {
+                    break;  
+                }
+            }
+            if (j * j > arr[i]) {
+                printf(" %d", arr[i]);  
+            } else {
+                printf(" #");         
+            }
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
